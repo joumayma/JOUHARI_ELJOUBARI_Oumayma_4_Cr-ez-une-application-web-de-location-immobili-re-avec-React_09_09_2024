c80c2b5a02c584e18374027aff551355bd7d946a
joumayma <jouhari.oumayma@gmail.com>
2024-09-23 09:28:22 +0200

    la création de la page d'accueil

--- src/components/pages/pages/Home.jsx
+++ src/components/pages/pages/Home.jsx
@@ -1,4 +1,6 @@
 import imgBanner from '../../../assets/images/bannerHome-kasa.webp';
+import Thumb from "../../../components/Thumb";
+import livres from "../../../Data/logements.json";
 
 const Home = () => {
     return (
@@ -6,6 +8,9 @@ const Home = () => {
         <div className="banner">
           <img src={imgBanner} alt="Chez vous, partout et ailleurs" />
         <h1>Chez vous, partout et ailleurs</h1>
+        </div>
+        <div className="gallery">
+        <Thumb livres={livres} />
         </div>
     
       </div>
